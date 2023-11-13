01b41b44dbd5e59820b59c1cd314eedfb11c0161
juanzuloaga <jzuloaga65@gmail.com>
2023-11-13 10:26:29 -0700

    Ultima revisión por Juan

--- to_run_intro_motivacional.R
+++ to_run_intro_motivacional.R
@@ -2,7 +2,7 @@
 install.packages("rmdformats")
 
 library(rmdformats)
-setwd("C:/Talleres_Ecodiversa_R/")
+setwd("C:/Talleres_R/")
 rmarkdown::render(input = "./Intro_Motivacional/Intro_Motivacional.Rmd",
                   output_file =  "Intro_Motivacional.html",
                   output_dir = "./Intro_Motivacional")
